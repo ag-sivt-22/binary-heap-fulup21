50ac46065440b945fcd59e8b83636583e71a62f4
fulup21 <147947142+fulup21@users.noreply.github.com>
2025-03-24 19:33:56 +0100

    upravena verze

--- binary_heap.py
+++ binary_heap.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 from typing import Any
 
-
 @dataclass
 class Element:
     value: Any
@@ -13,18 +12,18 @@ class BinaryHeap:
     def __init__(self):
         self.heap = []
 
-    def heap_check_up(self, idx: int) -> None:
+    def heap_check_up(self, idx:int)->None:
         if idx == 0:
-            return
-        parent = (idx - 1) // 2
-        if self.heap[idx].priority < self.heap[parent].priority:  # Min-heap (nejnižší priorita nahoře) vs Max-heap (nejvyšší priorita nahoře)
+            return None
+        parent = (idx-1) // 2
+        if self.heap[idx].priority < self.heap[parent].priority:
             self.heap[idx], self.heap[parent] = self.heap[parent], self.heap[idx]
             self.heap_check_up(parent)
 
-    def heap_check_down(self, idx: int) -> None:
+    def heap_check_down(self, idx:int)->None:
         left = 2 * idx + 1
         right = 2 * idx + 2
-        smallest = idx  # Min-heap
+        smallest = idx
 
         if left < len(self.heap) and self.heap[left].priority < self.heap[smallest].priority:
             smallest = left
@@ -36,23 +35,24 @@ class BinaryHeap:
             self.heap[idx], self.heap[smallest] = self.heap[smallest], self.heap[idx]
             self.heap_check_down(smallest)
 
-    def push(self, element: Element):
+    def push(self, element:Element):
         self.heap.append(element)
-        self.heap_check_up(len(self.heap) - 1)
+        self.heap_check_up(len(self.heap)-1)
 
-    def pop(self) -> Element:
+    def pop(self)->Element:
+        # najdi nejmensi prvek a vrat ho
         if not self.heap:
             raise Exception("Heap is empty")
 
         if len(self.heap) == 1:
             return self.heap.pop()
 
-        min_element = self.heap[0]  # Min-heap: nejmenší prvek je na vrcholu
+        smallest_element = self.heap[0]
         self.heap[0] = self.heap.pop()
         self.heap_check_down(0)
-        return min_element
+        return smallest_element
 
-    def head(self) -> Element:
+    def head(self)->Element:
         if not self.heap:
             raise Exception("Heap is empty")
         return self.heap[0]
