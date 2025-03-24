307a12496f70167908b50450f7f12729e256aae1
fulup21 <147947142+fulup21@users.noreply.github.com>
2025-03-24 13:15:07 +0100

    hotovy domaci ukol

--- binary_heap.py
+++ binary_heap.py
@@ -1,41 +1,58 @@
 from dataclasses import dataclass
 from typing import Any
 
+
 @dataclass
 class Element:
     value: Any
     priority: int
- 
+
 
 class BinaryHeap:
 
     def __init__(self):
         self.heap = []
-    
-    def push(self, element):
-        # naivni implementace binarni haldy
+
+    def heap_check_up(self, idx: int) -> None:
+        if idx == 0:
+            return
+        parent = (idx - 1) // 2
+        if self.heap[idx].priority < self.heap[parent].priority:  # Min-heap (nejnižší priorita nahoře)
+            self.heap[idx], self.heap[parent] = self.heap[parent], self.heap[idx]
+            self.heap_check_up(parent)
+
+    def heap_check_down(self, idx: int) -> None:
+        left = 2 * idx + 1
+        right = 2 * idx + 2
+        smallest = idx  # Min-heap
+
+        if left < len(self.heap) and self.heap[left].priority < self.heap[smallest].priority:
+            smallest = left
+
+        if right < len(self.heap) and self.heap[right].priority < self.heap[smallest].priority:
+            smallest = right
+
+        if smallest != idx:
+            self.heap[idx], self.heap[smallest] = self.heap[smallest], self.heap[idx]
+            self.heap_check_down(smallest)
+
+    def push(self, element: Element):
         self.heap.append(element)
+        self.heap_check_up(len(self.heap) - 1)
 
-    def pop(self):
-        # naivni implementace binarni haldy
-        # najdi nejmensi prvek a vrat ho
+    def pop(self) -> Element:
         if not self.heap:
             raise Exception("Heap is empty")
-        min_idx = 0
-        for idx in range(1,len(self.heap)):
-            if self.heap[idx].priority < self.heap[min_idx].priority:
-                min_idx = idx
-        return self.heap.pop(min_idx)
 
+        if len(self.heap) == 1:
+            return self.heap.pop()
 
-    def head(self):
+        min_element = self.heap[0]  # Min-heap: nejmenší prvek je na vrcholu
+        self.heap[0] = self.heap.pop()
+        self.heap_check_down(0)
+        return min_element
+
+    def head(self) -> Element:
         if not self.heap:
             raise Exception("Heap is empty")
-        # vrati nejmensi element ve fronte (element na cele fronty)
-        # protoze mame naivni implementaci, musime projit cely seznam
-        min_idx = 0
-        for idx in range(1,len(self.heap)):
-            if self.heap[idx].priority < self.heap[min_idx].priority:
-                min_idx = idx
-        return self.heap[min_idx]
-        
+        return self.heap[0]
